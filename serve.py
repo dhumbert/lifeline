65988f2329ad6ca293e15abca9f5d55206907fca
Devin Humbert <devin.humbert@gmail.com>
2014-03-16 22:04:42 -0700

    Made port configurable

--- serve.py
+++ serve.py
@@ -3,8 +3,13 @@
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
+from tornado.options import define, options
 from lifeline import app
 
+
+define("port", default=5999, help="run on the given port", type=int)
+options.parse_command_line()
+
 http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(5000)
+http_server.listen(options.port)
 IOLoop.instance().start()
